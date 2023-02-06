7fea4347b524a06b5caa93e1ac1bdcbc5040261e
Abu jafar sisty <124616816+Sisty37@users.noreply.github.com>
2023-02-06 23:45:44 +0600

    Add files via upload

---
+++ L.java
@@ -0,0 +1,15 @@
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicjavaa;
+
+/**
+ *
+ * @author ABU JAFAR SISTY
+ */
+public class L {
+    System.out.println("sisty \\536565\\Bsc in cse");
+}
