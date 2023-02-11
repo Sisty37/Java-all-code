eec756b7f609cdee94c23368247d1a8b3165bc33
Abu jafar sisty <124616816+Sisty37@users.noreply.github.com>
2023-02-11 19:09:01 +0600

    Update Test.java

--- Bank_Account/Test.java
+++ Bank_Account/Test.java
@@ -1,24 +1,38 @@
 
-package Bank_Account;
+package Bank_Account2;
 
 
 public class Test {
     public static void main(String[] args) {
         
+        System.out.println("========= Welcome to Islami Bank ========");
+        Bank_Account2 obj=new Bank_Account2();
         
-        	Bank_Account b=new Bank_Account();
-		System.out.println("=========Welcome to X bank==========");
-		b.setAccountNumber(1491533);
-		b.setAccountHolderName("Mr.X");
-		b.setBalance(500);
-		b.showDetails();
-		System.out.println("");
-        b.deposit(500);
-		System.out.println("");
-		b.withdraw(200);
-		System.out.println("");
-		System.out.println("Thank you for stay with us!!");
-		System.out.println("====================================");
+	obj.setAccountNumber(123456);
+        System.out.println("Account Number : "+obj.getAccountNumber());
+        
+        obj.setHoldername("sisty");
+        System.out.println("Account Holder name : "+obj.getHolderName());
+        
+        obj.setBalance(1000);
+        System.out.println("Your Previous Balance : "+obj.getBalance());
+        
+        
+        System.out.println(" ");
+        
+        obj.deposit(500);
+        
+	System.out.println("");
+        
+	obj.withdraw(200);
+        
+	System.out.println(" ");
+        
+	System.out.println("Thank you for stay with us!!");
+	System.out.println("====================================");
+		
+        
+        
+        
     }
-    
 }
