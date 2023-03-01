766a7e2fe0d6eacd29b69e78c5a9be030262294e
Abu jafar sisty <124616816+Sisty37@users.noreply.github.com>
2023-03-01 22:06:29 +0600

    Update Footballer.java

--- Inheritance_2/Footballer.java
+++ Inheritance_2/Footballer.java
@@ -14,9 +14,7 @@ public class Footballer extends Player {
 	
 	public Footballer(int goalScore, double ScoringAvg, String totalNumMatches)
 	{
-               super.getplayername();
-               super.getplayerage();
-               super.getplayercountry();
+              
 		this.goalScore=goalScore;
 		this.scoringAvg= ScoringAvg;
 		this.totalNumMatches=totalNumMatches;
@@ -34,7 +32,9 @@ public class Footballer extends Player {
 	
 	public void showFootballerInfo()
 	{
-		
+		super.getplayername();
+                super.getplayerage();
+                super.getplayercountry();
 		System.out.println("Goal Score:"+goalScore);
 		System.out.println("Avarege Score:"+scoringAvg);
 		System.out.println("Total number of match :"+totalNumMatches);
